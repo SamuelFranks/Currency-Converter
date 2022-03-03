9c5b4f3411a5c2ebd5481472a86d5bcbf71170e3
fruiticy <fruiticyTV@gmail.com>
2022-03-03 15:23:14 -0700

    Added base code

---
+++ CurrencyConverter.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class CurrencyConverter{
+    private static HttpURLConnection connection;
+    public static void main(String[]args){
+        Scanner scnr = new Scanner(System.in);
+        BufferedReader reader;
+        String line;
+        StringBuffer responseContent = new StringBuffer();
+        String currency1;
+        String currency2;
+
+        System.out.println("Hello! This is a currency converter using the Free Currency Rates API");
+        System.out.println("To get a full list of all the currencies converted go to: https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json");
+        System.out.println("Link to Free Currency Rates API used in this code: https://github.com/fawazahmed0/currency-api");
+        System.out.println("---------------------------------------------------------------------------------------------------------------------------------------------------");
+
+        System.out.println("Enter 3 letter version of currency you want to convert");
+        currency1 = scnr.next();
+        System.out.println("Enter 3 letter version of currency that you want it converted to");
+        currency2 = scnr.next();
+
+        try{URL url = new URL("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/"+ currency1 + "/" + currency2+ ".json");
+                connection = (HttpURLConnection) url.openConnection();
+
+                connection.setRequestMethod("GET");
+                connection.setConnectTimeout(5000);
+                connection.setReadTimeout(5000);
+
+                int status = connection.getResponseCode();
+                //System.out.println(status);
+
+                if(status > 299){
+                    reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+                    while((line = reader.readLine()) != null){
+                        responseContent.append(line);
+                    }
+                    reader.close();
+                }else{
+                    reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    while((line = reader.readLine()) != null){
+                        responseContent.append(line);
+                    }
+                    reader.close();
+                }
+                //System.out.println(responseContent.toString());
+
+                String requestResponse = responseContent.substring(37, responseContent.length() - 1);
+                connection.disconnect();
+                System.out.println(requestResponse + " " + currency2);
+            }catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+            scnr.close();
+    }
+}
